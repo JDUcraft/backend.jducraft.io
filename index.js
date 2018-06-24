4a7e00aadc6e02808d75e5b08acc2ec194b88940
Jonathan Duberville <jduberville@octo.com>
2018-06-24 22:07:33 +0200

    feature: added hotspot-analyzer route

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-'use strict';
+import Hapi from 'hapi';
 
-const Hapi=require('hapi');
-
-const server=Hapi.server({
-	    host:'localhost',
-	    port:4000
+const server = Hapi.server({
+    host: 'localhost',
+    port: 4000,
 });
 
 server.route({
-	method:'GET',
-	path:'/',
-	handler: function (request, h) {
-		return {message: "Welcome to JDUCraft's Backend :)"};
-	}
+    method: 'GET',
+    path: '/',
+    handler() {
+        return {
+            message: 'Welcome to JDUCraft\'s Backend :)',
+            availableRoutes: [
+                '/hotspot-analyzer',
+            ],
+        };
+    },
+});
+
+server.route({
+    method: 'GET',
+    path: '/hotspot-analyzer',
+    handler() {
+        return {
+            message: 'Authentication Service',
+        };
+    },
 });
 
 async function start() {
-	try {
-		await server.start();
-	}
-	catch (err) {
-		console.log(err);
-		process.exit(1);
-	}
-	console.log('Server running at:', server.info.uri);
-};
+    try {
+        await server.start();
+    } catch (err) {
+        process.exit(1);
+    }
+    console.log('Server running at:', server.info.uri);
+}
 
 start();
